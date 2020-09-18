bd8665103990bae77a9be155c4d94ff6a6f28f18
kathy0917 <kathy95917@naver.com>
2020-09-19 00:02:02 +0900

    install babel

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require("express"); //express를 불러옴
+import express from "express";
+//윗줄이 최신문법. const express = require("express"); //express를 불러옴
+//babel이 es6문법을 과거 버전으로 변형해줌.
+
 const { response } = require("express");
 const app = express(); // express를 실행
 
@@ -8,12 +11,9 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-function handleHome(req, res) {
-  //console.log(req);
-  res.send("Hello from home");
-}
+const handleHome = (request, reponse) => response.send("Hello from home");
 
-//main url로 접근시
+//main url로 접근시(route생성) handleHome응답
 app.get("/", handleHome);
 //listening하기 시작할 때 handleListening호출
 app.listen(PORT, handleListening);
