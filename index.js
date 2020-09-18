f110099375ee9cbf50021b104f9195a1f5b8167f
kathy0917 <kathy95917@naver.com>
2020-09-19 00:31:56 +0900

    add nodemoon

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from "express";
 //윗줄이 최신문법. const express = require("express"); //express를 불러옴
 //babel이 es6문법을 과거 버전으로 변형해줌.
 
-const { response } = require("express");
+/* dependencies는 프로젝트가 필요한 것,
+   devDependencies는 개발자가 필요한 것
+   npm install [package명] -D로 설치 가능 */
+
+/*nodemon은 코드 수정 시 수동으로 서버를 시작해야하는 번거로움을 해결하는 패키지
+nodemon app crashed error -> 작업관리자에서 node.js:server-side종료한 뒤 재실행*/
 const app = express(); // express를 실행
 
 const PORT = 4000;
@@ -11,7 +16,7 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-const handleHome = (request, reponse) => response.send("Hello from home");
+const handleHome = (request, response) => response.send("Hello from home");
 
 //main url로 접근시(route생성) handleHome응답
 app.get("/", handleHome);
